a10edea485e960b626313d361f55dab57a1786e6
Feytox <feytox@vk.com>
2023-05-06 14:23:09 +0500

    Initial commit

---
+++ src/main/java/ru/feytox/devoptimized/DevOptimized.java
@@ -0,0 +1,15 @@
+package ru.feytox.devoptimized;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DevOptimized implements ClientModInitializer {
+    public static final String MOD_ID = "devoptimized";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.warn("If you see this message outside of the IDE, you must uninstall DevOptimized and install all the mods included in it yourself.");
+    }
+}
